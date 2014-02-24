c5d3a0aa14b32ea93db7b041dd7a84dd6dc205e1
Victor Kotseruba <barbuzaster@gmail.com>
2014-02-24 20:35:19 +0400

    fotorama

--- main.js
+++ main.js
@@ -1,33 +1,32 @@
 (function() {
-  var loadImage;
-
-  loadImage = function(url, callback) {
-    var img;
-    img = document.createElement("img");
-    img.src = url;
-    return img.onload = function() {
-      var height, width;
-      $(img).css("visibility", "hidden");
-      $(img).css("position", "absolute");
-      document.body.appendChild(img);
-      width = $(img).width();
-      height = $(img).height();
-      document.body.removeChild(img);
-      return callback(img, width, height);
-    };
-  };
-
   $("#clients a").each(function() {
     var a, url;
     a = $(this);
     url = "images/clients/" + (a.data('client')) + ".png";
-    return loadImage(url, function(_, width, height) {
-      a.css({
-        width: width,
-        height: height,
-        "background-image": "url(" + url + ")"
-      });
-      return a.attr("data-client", null);
+    a.append("<img src='" + url + "'>");
+    return a.attr("data-client", null);
+  });
+
+  $("#reviews ul li").each(function() {
+    var li, logoUrl, photoUrl;
+    li = $(this);
+    photoUrl = "images/reviews/" + (li.data('client')) + "-photo.jpg";
+    logoUrl = "images/reviews/" + (li.data('client')) + "-logo.png";
+    li.css("background-image", "url(" + photoUrl + ")");
+    return li.find(".person").css("background-image", "url(" + logoUrl + ")");
+  });
+
+  $("#reviews ol li").each(function() {
+    var idx;
+    idx = $(this).index();
+    return $(this).click(function() {
+      var slide;
+      $(this).siblings().removeClass("active");
+      $(this).addClass("active");
+      slide = $("#reviews ul li").get(idx);
+      $(slide).prevAll().removeClass("right").addClass("left");
+      $(slide).nextAll().removeClass("left").addClass("right");
+      return $(slide).removeClass("left").removeClass("right");
     });
   });
 
